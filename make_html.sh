1d6bbbee5a5fee530054a1a379000633ece5a03d
Kota Shiratsuka <kota@insaneworks.co.jp>
2021-12-23 18:45:58 +0900

    git worktreeを使ってhtml生成できるようパス変更

--- make_html.sh
+++ make_html.sh
@@ -5,12 +5,12 @@ IFS="
 
 for i in `cat 2020NTT_redirect.txt | sed 's%https://2020.ntt/%%g' | grep -v "html "`
 do
-	[ -d ${i%% *} ] || mkdir -p ${i%% *}
-	cat source.html | sed "s%TO%${i##* }%" > ${i%% *}index.html
+	[ -d html/${i%% *} ] || mkdir -p html/${i%% *}
+	cat source.html | sed "s%TO%${i##* }%" > html/${i%% *}index.html
 done
 
 for i in `cat 2020NTT_redirect.txt | sed 's%https://2020.ntt/%%g' | grep "html "`
 do
-	[ -d `dirname ${i%% *}` ] || mkdir -p `dirname ${i%% *}`
-	cat source.html | sed "s%TO%${i##* }%" > ${i%% *}
+	[ -d `dirname html/${i%% *}` ] || mkdir -p html/`dirname ${i%% *}`
+	cat source.html | sed "s%TO%${i##* }%" > html/${i%% *}
 done
