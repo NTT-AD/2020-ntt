55ad49a959d54f4a17871a9f9cc838b7d931b06e
Kota Shiratsuka <kota@insaneworks.co.jp>
2021-12-24 20:16:20 +0900

    TOに同じドメインが含まれても動作するようにした

--- make_html.sh
+++ make_html.sh
@@ -2,14 +2,16 @@
 
 OUTPUT_DIR=$1
 
-cat 2020NTT_redirect.txt | sed 's%https://2020.ntt/%%g' | grep -v "html " | \
+cat 2020NTT_redirect.txt | grep -v "html " | \
 while IFS=" " read FROM TO; do
-	[ -d $OUTPUT_DIR/$FROM ] || mkdir -p $OUTPUT_DIR/$FROM
-	cat source.html | sed "s%TO%${TO}%" > $OUTPUT_DIR/${FROM}index.html
+	FROM_WO_DOMAIN=`echo $FROM | sed 's%https://2020.ntt/%%g'`
+	[ -d ${OUTPUT_DIR}/${FROM_WO_DOMAIN} ] || mkdir -p ${OUTPUT_DIR}/${FROM_WO_DOMAIN}
+	cat source.html | sed "s%TO%${TO}%" > ${OUTPUT_DIR}/${FROM_WO_DOMAIN}index.html
 done
 
-cat 2020NTT_redirect.txt | sed 's%https://2020.ntt/%%g' | grep "html " | \
+cat 2020NTT_redirect.txt | grep "html " | \
 while IFS=" " read FROM TO; do
-	[ -d `dirname $OUTPUT_DIR/$FROM` ] || mkdir -p `dirname $OUTPUT_DIR/$FROM`
-	cat source.html | sed "s%TO%${TO}%" > $OUTPUT_DIR/${FROM}
+	FROM_WO_DOMAIN=`echo $FROM | sed 's%https://2020.ntt/%%g'`
+	[ -d `dirname ${OUTPUT_DIR}/${FROM_WO_DOMAIN}` ] || mkdir -p `dirname ${OUTPUT_DIR}/${FROM_WO_DOMAIN}`
+	cat source.html | sed "s%TO%${TO}%" > ${OUTPUT_DIR}/${FROM_WO_DOMAIN}
 done
